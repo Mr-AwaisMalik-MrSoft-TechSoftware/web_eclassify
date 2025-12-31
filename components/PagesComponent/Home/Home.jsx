53ccfae0b8534a6fd35d6008f2f1f2bd9d6d2d24
Mr-AwaisMalik-MrSoft-TechSoftware <78647994+Mr-AwaisMalik-MrSoft-TechSoftware@users.noreply.github.com>
2025-12-31 16:05:46 +0500

    Update Home.jsx

--- components/PagesComponent/Home/Home.jsx
+++ components/PagesComponent/Home/Home.jsx
@@ -1,34 +1,40 @@
 "use client";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import AllItems from "./AllItems";
 import FeaturedSections from "./FeaturedSections";
 import OfferSlider from "./OfferSlider";
-import { FeaturedSectionApi, sliderApi } from "@/utils/api";
-import { getCurrentLangCode } from "@/redux/reducer/languageSlice";
-import { useSelector } from "react-redux";
-import { getCityData, getKilometerRange } from "@/redux/reducer/locationSlice";
-import OfferSliderSkeleton from "@/components/PagesComponent/Home/OfferSliderSkeleton";
+import OfferSliderSkeleton from "./OfferSliderSkeleton";
 import FeaturedSectionsSkeleton from "./FeaturedSectionsSkeleton";
 import PopularCategories from "./PopularCategories";
+import { FeaturedSectionApi, sliderApi } from "@/utils/api";
+import { getCurrentLangCode } from "@/redux/reducer/languageSlice";
+import { getCityData, getKilometerRange } from "@/redux/reducer/locationSlice";
 
 const Home = () => {
   const KmRange = useSelector(getKilometerRange);
   const cityData = useSelector(getCityData);
   const currentLanguageCode = useSelector(getCurrentLangCode);
+
   const [IsFeaturedLoading, setIsFeaturedLoading] = useState(false);
   const [featuredData, setFeaturedData] = useState([]);
   const [Slider, setSlider] = useState([]);
   const [IsSliderLoading, setIsSliderLoading] = useState(true);
-  const allEmpty = featuredData?.every((ele) => ele?.section_data.length === 0);
 
+  // Safe check if all featured sections are empty
+  const allEmpty = featuredData?.every(
+    (ele) => (ele?.section_data || []).length === 0
+  );
+
+  // Fetch Slider
   useEffect(() => {
     const fetchSliderData = async () => {
       try {
         const response = await sliderApi.getSlider();
-        const data = response.data;
-        setSlider(data.data);
+        const sliderArray = response.data?.data || [];
+        setSlider(sliderArray);
       } catch (error) {
-        console.error("Error:", error);
+        console.error("Error fetching slider data:", error);
       } finally {
         setIsSliderLoading(false);
       }
@@ -36,48 +42,46 @@ const Home = () => {
     fetchSliderData();
   }, []);
 
+  // Fetch Featured Sections
   useEffect(() => {
     const fetchFeaturedSectionData = async () => {
       setIsFeaturedLoading(true);
       try {
         const params = {};
+
         if (Number(KmRange) > 0) {
           params.radius = KmRange;
           params.latitude = cityData.lat;
           params.longitude = cityData.long;
         } else {
-          if (cityData?.areaId) {
-            params.area_id = cityData.areaId;
-          } else if (cityData?.city) {
-            params.city = cityData.city;
-          } else if (cityData?.state) {
-            params.state = cityData.state;
-          } else if (cityData?.country) {
-            params.country = cityData.country;
-          }
+          if (cityData?.areaId) params.area_id = cityData.areaId;
+          else if (cityData?.city) params.city = cityData.city;
+          else if (cityData?.state) params.state = cityData.state;
+          else if (cityData?.country) params.country = cityData.country;
         }
+
         const response = await FeaturedSectionApi.getFeaturedSections(params);
-        const { data } = response.data;
-        setFeaturedData(data);
+        const featuredArray = response.data?.data || [];
+        setFeaturedData(featuredArray);
       } catch (error) {
-        console.error("Error:", error);
+        console.error("Error fetching Featured Sections:", error);
       } finally {
         setIsFeaturedLoading(false);
       }
     };
     fetchFeaturedSectionData();
   }, [cityData.lat, cityData.long, KmRange, currentLanguageCode]);
+
   return (
     <>
       {IsSliderLoading ? (
         <OfferSliderSkeleton />
       ) : (
-        Slider &&
-        Slider.length > 0 && (
-          <OfferSlider Slider={Slider} IsLoading={IsSliderLoading} />
-        )
+        Slider?.length > 0 && <OfferSlider Slider={Slider} IsLoading={IsSliderLoading} />
       )}
+
       <PopularCategories />
+
       {IsFeaturedLoading ? (
         <FeaturedSectionsSkeleton />
       ) : (
@@ -87,6 +91,7 @@ const Home = () => {
           allEmpty={allEmpty}
         />
       )}
+
       <AllItems cityData={cityData} KmRange={KmRange} />
     </>
   );
